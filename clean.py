8b39f714a41edd82a9a4f6120a71e247da952217
Yutaka Kobayashi <48763691+kobayashiyutaka@users.noreply.github.com>
2021-08-02 10:22:40 +0900

    Update clean.py count.py
    Drafts of answers

--- clean.py
+++ clean.py
@@ -1,1 +1,12 @@
-# 以下にコードを記述してください。
+# %%
+with open('./oz.md','r') as file:
+    text = file.read()
+
+trans_rule = {'.':'',',':'','"':'',';':'','#':'',':':'','!':''}
+trans = str.maketrans(trans_rule)
+text_cleaned = text.translate(trans).casefold()
+
+with open('./oz_cleaned.md','w') as file:
+    file.write(text_cleaned)
+
+# %%
--- count.py
+++ count.py
@@ -1,1 +1,24 @@
-# 以下にコードを記述してください。
+# %%
+with open('./oz_cleaned.md','r') as file:
+    text = file.read()
+
+words = text.split()
+count = dict()
+
+for w in words:
+    if w in count.keys():
+        count[w] += 1
+    else:
+        count[w] = 1
+
+keys = list(count.keys())
+keys.sort()
+
+output_lines = ['# Word frequencies in chapter 1\n\n',
+'|word|count|\n','|--|--|\n']
+
+for key in keys:
+    output_lines.append('|'+key+'|'+str(count[key])+'|\n')
+
+with open('./word_count.md','w') as file:
+    file.writelines(output_lines)
