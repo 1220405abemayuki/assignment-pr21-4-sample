22eaeda51918c6b5540414e5f81522976cb65beb
Yutaka Kobayashi <48763691+kobayashiyutaka@users.noreply.github.com>
2021-08-02 13:48:47 +0900

    Add clean.py,count.py,oz_cleaned.md,word_count.md

--- clean.py
+++ clean.py
@@ -2,8 +2,7 @@
 with open('./oz.md','r') as file:
     text = file.read()
 
-trans_rule = {'.':'',',':'','"':'',';':'','#':'',':':'','!':''}
-trans = str.maketrans(trans_rule)
+trans = str.maketrans(dict.fromkeys('#.,":;!',''))
 text_cleaned = text.translate(trans).casefold()
 
 with open('./oz_cleaned.md','w') as file:
--- count.py
+++ count.py
@@ -2,7 +2,15 @@
 with open('./oz_cleaned.md','r') as file:
     text = file.read()
 
+# テキストの単語への分割
 words = text.split()
+
+# 各単語の出現回数を格納した辞書の作成
+# Pythonっぽいやり方
+count = {w:words.count(w) for w in set(words)}
+
+# 泥臭いやり方
+'''
 count = dict()
 
 for w in words:
@@ -10,15 +18,18 @@ for w in words:
         count[w] += 1
     else:
         count[w] = 1
+'''
 
-keys = list(count.keys())
-keys.sort()
-
+# 出力する行のリスト(まずは最初の3行)
 output_lines = ['# Word frequencies in chapter 1\n\n',
 '|word|count|\n','|--|--|\n']
 
-for key in keys:
+# 出力する行のリストに足していく
+# 内包表記でもできるがforループのほうが見やすいかも
+for key in sorted(count.keys()):
     output_lines.append('|'+key+'|'+str(count[key])+'|\n')
 
 with open('./word_count.md','w') as file:
     file.writelines(output_lines)
+
+# %%
